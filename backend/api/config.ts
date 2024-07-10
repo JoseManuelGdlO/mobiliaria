722bfa7c9b728f721a2da65f454073a0c5b53842
Jose Manuel <josemanuelgarciadelao@gmail.com>
2024-07-10 09:48:50 -0600

    feat: change db

--- backend/api/config.ts
+++ backend/api/config.ts
@@ -6,10 +6,10 @@ export const config = {
         database: "base",
     },
     dbDurangeneidad: {
-        host: "82.180.152.204",
-        user: "u427127638_admin",
-        password: "Admin020424",
-        database: "u427127638_durangeneidad",
+        host: "database-1.cxcex64atlct.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "%7lzh(brYPE{{F]r.Og37!1!t-}E",
+        database: "dgo",
     },
     listPerPage: 10,
 };
