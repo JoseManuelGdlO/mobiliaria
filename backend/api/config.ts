00f0c7bc1c101809575995cc8273b4da34e6e7f2
Jose Manuel <josemanuelgarciadelao@gmail.com>
2024-07-15 20:15:45 -0600

    feat: change p[assword

--- backend/api/config.ts
+++ backend/api/config.ts
@@ -2,13 +2,13 @@ export const config = {
     db: {
         host: "database-1.cxcex64atlct.us-east-1.rds.amazonaws.com",
         user: "admin",
-        password: "%7lzh(brYPE{{F]r.Og37!1!t-}E",
+        password: "F}6MtB1Le+TGKgc<.N#wg0F8[x<1",
         database: "base",
     },
     dbDurangeneidad: {
         host: "database-1.cxcex64atlct.us-east-1.rds.amazonaws.com",
         user: "admin",
-        password: "%7lzh(brYPE{{F]r.Og37!1!t-}E",
+        password: "F}6MtB1Le+TGKgc<.N#wg0F8[x<1",
         database: "dgo",
     },
     listPerPage: 10,
