c7d3bf21d3f6e60a5b5c625a99007f8865de7fa4
Jose Manuel <josemanuelgarciadelao@gmail.com>
2025-05-20 01:20:29 -0600

    add new bd

--- backend/api/config.ts
+++ backend/api/config.ts
@@ -1,12 +1,12 @@
 export const config = {
     db: {
-        host: "database-1.cxcex64atlct.us-east-1.rds.amazonaws.com",
+        host: "eventivapp.cnay0qcmgf10.us-east-2.rds.amazonaws.com",
         user: "admin",
         password: "8a4aAsQ3:WHxVDtZ]R)pJ(KYtBQ?",
         database: "base",
     },
     dbDurangeneidad: {
-        host: "database-1.cxcex64atlct.us-east-1.rds.amazonaws.com",
+        host: "eventivapp.cnay0qcmgf10.us-east-2.rds.amazonaws.com",
         user: "admin",
         password: "8a4aAsQ3:WHxVDtZ]R)pJ(KYtBQ?",
         database: "dgo",
