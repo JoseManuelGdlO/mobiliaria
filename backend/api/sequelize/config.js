31532f1511fcd60a8eb7c67a7b23fbc9e475b853
Alka1227 <javier_karim@outlook.com>
2026-08-06 10:26:14 -0600

    Chore: Remove deprecated and /dist file delete .env file and related configuration scripts from the backend API. Update .gitignore to exclude .env and distribution files, ensuring sensitive data is not tracked. This cleanup enhances project maintainability and security.

--- backend/api/sequelize/config.js
+++ backend/api/sequelize/config.js
@@ -8,17 +8,17 @@ const shared = {
 module.exports = {
   development_main: {
     ...shared,
-    host: process.env.DB_HOST || "eventivapp.cnay0qcmgf10.us-east-2.rds.amazonaws.com",
-    username: process.env.DB_USER || "admin",
-    password: process.env.DB_PASSWORD || "8a4aAsQ3:WHxVDtZ]R)pJ(KYtBQ?",
-    database: process.env.DB_NAME || "base",
+    host: process.env.DB_HOST || "",
+    username: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "",
   },
   development_dgo: {
     ...shared,
-    host: process.env.DB_DGO_HOST || "eventivapp.cnay0qcmgf10.us-east-2.rds.amazonaws.com",
-    username: process.env.DB_DGO_USER || "admin",
-    password: process.env.DB_DGO_PASSWORD || "8a4aAsQ3:WHxVDtZ]R)pJ(KYtBQ?",
-    database: process.env.DB_DGO_NAME || "dgo",
+    host: process.env.DB_DGO_HOST || "",
+    username: process.env.DB_DGO_USER || "",
+    password: process.env.DB_DGO_PASSWORD || "",
+    database: process.env.DB_DGO_NAME || "",
   },
   production_main: {
     ...shared,
